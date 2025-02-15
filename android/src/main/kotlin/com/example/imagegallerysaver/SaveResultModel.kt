9f0a0a3393d752cb5317ef5acaf616e256ae29eb
Gnayoah <haoyang88@88.com>
2025-02-14 21:14:08 -0800

    Create SaveResultModel.kt

---
+++ android/src/main/kotlin/com/example/imagegallerysaver/SaveResultModel.kt
@@ -0,0 +1,13 @@
+package com.example.imagegallerysaver
+
+class SaveResultModel(var isSuccess: Boolean,
+                      var filePath: String? = null,
+                      var errorMessage: String? = null) {
+    fun toHashMap(): HashMap<String, Any?> {
+        val hashMap = HashMap<String, Any?>()
+        hashMap["isSuccess"] = isSuccess
+        hashMap["filePath"] = filePath
+        hashMap["errorMessage"] = errorMessage
+        return hashMap
+    }
+}
